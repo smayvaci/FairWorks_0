ca786cc87d3a0586615aad78681db49823020504
Fatma Çetin <granze_fatma88@hotmail.com>
2023-06-19 15:12:32 +0300

    Project.VM içerisine PureVMs kalsörü açıldı. Project.BLL katmanında DesignPatterns Klasörü içerisine Singleton Pattern classı açıldı. Repositories klasörünün içerisine Repositories le ilgili işlemler yapılacak

---
+++ Project.BLL/DesignPatterns/SingletonPattern/DBTool.cs
@@ -0,0 +1,26 @@
+﻿using Project.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPatterns.SingletonPattern
+{// singleton pattern bir design patterdir. Repository bir design pattern olarak kabul edilmemiştir. Çünkü farklı projelerde değişir. Repositories olarak yer almaz. 
+    public class DBTool
+    {
+        public DBTool()
+        {
+
+        }
+        static MyContext _dbInstance;
+        public static MyContext DbInstance
+        { 
+            get
+            {
+                if (_dbInstance == null) _dbInstance = new MyContext();
+                return _dbInstance; 
+            }
+        }
+    }
+}
